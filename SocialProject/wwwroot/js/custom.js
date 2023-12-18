559a79e6ffc0daf57966c79b75e8492a26187175
sevilsariyeva00 <sevilsariyeva00@gmail.com>
2023-12-19 02:03:25 +0400

    .

--- SocialProject/wwwroot/js/custom.js
+++ SocialProject/wwwroot/js/custom.js
@@ -1,24 +1,24 @@
-﻿$(function () {
-	$('[data-skin]').on('click', function (e) {
+$(function() {
+	$('[data-skin]').on('click', function(e) {
 		e.preventDefault();
 		var skin = $(this).data('skin');
-		$('#style-skin').attr('href', 'assets/css/skin-' + skin + '.css');
+		$('#style-skin').attr('href', 'assets/css/skin-'+ skin +'.css');
 	});
 
 	// Sidebar-boxed: Try it section
-	$('#sb-left-side').on('click', function () {
+	$('#sb-left-side').on('click', function() {
 		$('.sidebar-boxed').removeClass('sidebar-right');
 	});
 
-	$('#sb-right-side').on('click', function () {
+	$('#sb-right-side').on('click', function() {
 		$('.sidebar-boxed').addClass('sidebar-right');
 	});
 
-	$('#sb-skin-light').on('click', function () {
+	$('#sb-skin-light').on('click', function() {
 		$('.sidebar-boxed').removeClass('sidebar-dark');
 	});
 
-	$('#sb-skin-dark').on('click', function () {
+	$('#sb-skin-dark').on('click', function() {
 		$('.sidebar-boxed').addClass('sidebar-dark');
 	});
 });
