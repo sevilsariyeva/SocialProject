ef605a23aea6f42636bb2a4e2f6476946fdd7e83
sevilsariyeva00 <sevilsariyeva00@gmail.com>
2023-12-22 01:26:13 +0400

    .

---
+++ SocialProject/Controllers/FeedController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialProject.WebUI.Controllers
+{
+    public class FeedController:Controller
+    {
+        public IActionResult Weather()
+        {
+            return View("Weather");
+        }
+    }
+}
